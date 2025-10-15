2b7f1ebc141323de265d70478dd42df6afb0cd28
JinKun11605 <jinkun.116.05@gmail.com>
2025-10-15 22:44:28 +0700

    search

--- routes/client/home.route.ts
+++ routes/client/home.route.ts
@@ -4,5 +4,6 @@ import * as homeController from "../../controllers/client/home.controller";
 const router = express.Router();
 
 router.get("/", homeController.index);
+router.get("/search", homeController.search);
 
 export default router;
