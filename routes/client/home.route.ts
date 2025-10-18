f47789f612b2a69bca20063228bc419b67ae056b
JinKun11605 <jinkun.116.05@gmail.com>
2025-10-19 00:00:56 +0700

    Thể thao / Sale

--- routes/client/home.route.ts
+++ routes/client/home.route.ts
@@ -5,5 +5,7 @@ const router = express.Router();
 
 router.get("/", homeController.index);
 router.get("/search", homeController.search);
+router.get("/sale", homeController.sale);
+router.get("/the-thao", homeController.theThao);
 
 export default router;
